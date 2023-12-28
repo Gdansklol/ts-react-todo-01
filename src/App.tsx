0e4f8b57db0c06afeb9f2ed22335fbfb216e1e54
swedenebomi <swedenebomi@gamil.com>
2023-12-28 01:52:44 +0100

    added App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-const App: React.FC =() =>{
-  return (
+const App: React.FC = ()=> {
+  return(
     <div className="App">
-      <div className="heading">To Do Task </div>
+      <span className="heading">ToDo Task</span>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+// import React from 'react';
+// import './App.css';
+
+// const App: React.FC =() =>{
+//   return (
+//     <div className="App">
+//       <div className="heading">To Do Task </div>
+//     </div>
+//   );
+// }
+
+// export default App;
