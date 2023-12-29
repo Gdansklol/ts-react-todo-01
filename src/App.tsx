296e96180683450c4881686387ad5583d168e55c
swedenebomi <swedenebomi@gamil.com>
2023-12-29 04:13:35 +0100

    model

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React,{useState} from "react";
 import "./App.css";
 import InputFeild from "./components/InputFeild";
+import {Todo} from "./model";
 
 const App: React.FC = ()=> {
 // const [todo, setTodo] = useState<string | number>("");
 const [todo, setTodo] = useState<string>("");
+const [todos, setTodos] = useState<Todo[]>([]);
 
 console.log(todo);
 
